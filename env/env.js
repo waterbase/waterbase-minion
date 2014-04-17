b640a2bc39ad99555c2ffd73df3077f724ff50fe
wjmao88 <wjmao88@gmail.com>
2014-04-16 22:39:21 -0700

    cleaned up all tarball code

--- env/env.js
+++ env/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoHost: 'mongodb://localhost/',
+  mongoHost: 'mongodb://heroku:FChOaJPn4Uqjl-4kheh0kSNlYZILVTnfGqQVTR2uMRbghI6NV_2LST-H0IrfFbKF-Myy6iBdiPK1Ksk1nN7rDw@oceanic.mongohq.com:10010/app23669276/',
   masterUrl: 'waterbase.herokuapp.com/minion',
   masterHost: 'waterbase.herokuapp.com',
   port: process.env.port || 12345
